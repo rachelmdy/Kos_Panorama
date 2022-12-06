7fc09ca912aba910055d5a0338d8e72cd8daef56
rachelmdy <93273724+rachelmdy@users.noreply.github.com>
2022-12-06 16:18:06 +0700

    tugas crud ionic

--- Kos_Panorama/database/kos_panorama.sql
+++ Kos_Panorama/database/kos_panorama.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2022 at 07:47 AM
+-- Generation Time: Dec 06, 2022 at 10:08 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `kos_alden`
+-- Database: `kos_panorama`
 --
 
 -- --------------------------------------------------------
@@ -31,8 +31,17 @@ CREATE TABLE `penghuni` (
   `id` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `nomor_kamar` int(11) NOT NULL,
-  `no_tlp` int(12) NOT NULL
+  `no_tlp` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `penghuni`
+--
+
+INSERT INTO `penghuni` (`id`, `nama`, `nomor_kamar`, `no_tlp`) VALUES
+(1, 'Nurman', 1, '21982718921'),
+(2, 'Musa', 2, '0823374658976'),
+(3, 'Rachel', 30, '085261837367');
 
 -- --------------------------------------------------------
 
@@ -50,7 +59,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`username`, `password`) VALUES
-('rachel', '4e2fe6dc5c296a7a548a7fd4eaf3ad07');
+('admin', '21232f297a57a5a743894a0e4a801fc3'),
+('susi', 'susi'),
+('tes', 'b93939873fd4923043b9dec975811f66');
 
 --
 -- Indexes for dumped tables
@@ -63,6 +74,12 @@ ALTER TABLE `penghuni`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -70,7 +87,7 @@ ALTER TABLE `penghuni`
 -- AUTO_INCREMENT for table `penghuni`
 --
 ALTER TABLE `penghuni`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
